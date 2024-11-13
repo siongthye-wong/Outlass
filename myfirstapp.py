aaf6127e6de36bf264bf1ccadfc0ca1ef1fe0b6a
siongthye-wong <jojoba038@gmail.com>
2024-11-13 15:52:33 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -30,3 +30,17 @@ elif option=='T n C':
           'Attendance Status': ['yes', 'yes', 'yes', 'no']
         }))
 
+
+elif option=='Long Process':
+    'Starting a long computation...'
+
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
+
