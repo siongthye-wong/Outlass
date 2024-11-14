e0f5b772ac768676c276d414917364aee5495c57
siongthye-wong <jojoba038@gmail.com>
2024-11-14 10:26:05 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -6,5 +6,5 @@ import pickle
 st.write("# Advertising Sales Report")
 st.write("This web shows the total sales of different advertisting tool!")
 
-st.sidebar.header('User Input Parameters')
+st.sidebar.header('Input Parameters')
 
