5c3dff5f4f59d7915c2911770f0709177724e6e0
siongthye-wong <jojoba038@gmail.com>
2024-11-14 10:31:56 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -12,10 +12,11 @@ def user_input_features():
     television = st.sidebar.slider('Television', 4.3, 7.9, 5.4)
     newspapers = st.sidebar.slider('Newspapers', 2.0, 4.4, 3.4)
     radio = st.sidebar.slider('Radio', 1.0, 6.9, 1.3)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,}
+    data = {'Televeision': television,
+            'Newspaper': newspapers,
+            'Radio': radio,}
     features = pd.DataFrame(data, index=[0])
     return features
 
 df = user_input_features()
+
