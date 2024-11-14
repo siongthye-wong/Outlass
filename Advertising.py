6d790d441b181a24f881a5d2679d8493d4dc35f2
siongthye-wong <jojoba038@gmail.com>
2024-11-14 10:24:29 +0800

    Add files via upload

---
+++ Advertising.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+st.write("# Advertising Sales Report")
+st.write("This web shows the total sales of different advertisting tool!")
+
+st.sidebar.header('User Input Parameters')
+
