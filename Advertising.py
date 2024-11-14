94fd846174c78ae4df7e41039272954642114fac
siongthye-wong <jojoba038@gmail.com>
2024-11-14 14:20:59 +0800

    Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -48,3 +48,48 @@ import pickle
 pickle.dump(modelSvr, open("modeladvertising.h5", "wb")) #wb: write binary
 
 print("Model saved. Please download the file.")
+
+
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+from sklearn.naive_bayes import GaussianNB
+
+st.write("#Advertising Sales Priduction")
+st.write("This app predicts the **Advertising** type")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    television = st.sidebar.slider('Television', 0, 150, 300)
+    radio = st.sidebar.slider('Radio', 0, 150, 300)
+    newspaper = st.sidebar.slider('Newspaper', 0, 150, 300)
+    data = {'Television': television,
+            'Radio': radio,
+            'Newspaper': newspaper,}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+data = sns.load_dataset('advertising')
+X = data.drop(['type'],axis=1)
+Y = data.species.copy()
+
+modelGaussianAdvertising = GaussianNB()
+modelGaussianAdvertising.fit(X, Y)
+
+prediction = modelGaussianAdvertising.predict(df)
+prediction_proba = modelGaussianAdvertising.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(Y.unique())
+
+st.subheader('Prediction')
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
