09855d3a69dda98fdafec57d3687f5482340fdc5
siongthye-wong <jojoba038@gmail.com>
2024-11-14 10:30:06 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -8,3 +8,14 @@ st.write("This web shows the total sales of different advertisting tool!")
 
 st.sidebar.header('Input Parameters')
 
+def user_input_features():
+    television = st.sidebar.slider('Television', 4.3, 7.9, 5.4)
+    newspapers = st.sidebar.slider('Newspapers', 2.0, 4.4, 3.4)
+    radio = st.sidebar.slider('Radio', 1.0, 6.9, 1.3)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
